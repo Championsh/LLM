4cd68399f3a46f32dd7ecf75c05b5cb746b1f46c
Andrey Mitrofanov <andrey20031707@mail.ru>
2023-11-01 18:23:59 +0300

    Some New Data

--- code-llama-34b.py
+++ code-llama-34b.py
@@ -8,7 +8,7 @@ tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
-    torch_dtype=torch.float16,
+    torch_dtype=torch.float32,
     device_map="auto",
 )
 
