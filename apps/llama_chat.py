e2ff1edabf7337e7797ade9912a5ff9b8ba0c074
SecureSight0 <securesight@mail.ru>
2023-12-08 16:29:03 +0300

    Use Llama-70b + Enable System Prompt

--- apps/llama_chat.py
+++ apps/llama_chat.py
@@ -6,48 +6,64 @@ import sys
 import os
 
 
-def generate_response(prompt_input, email, passwd):
+def generate_response(prompt_input, email, passwd, sys_prompt=""):
     # Hugging Face Login
     start_time = time.time()
 
     sign = Login(email, passwd)
     cookies = sign.login()
     # Create ChatBot
-    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-    print(chatbot.chat(prompt_input))
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict(), default_llm=1,
+                              system_prompt=sys_prompt)
+    output = chatbot.query(prompt_input, max_new_tokens=4096)
+    print(output)
 
     end_time = time.time()
     print('TIME SPENT: %.1f s.' % (end_time - start_time))
     print('----------\n')
 
 
-def run(dir_path=None):
+def run(dir_path=None, sys_prompt_flag=False):
     secrets = dotenv_values("hf.env")
     hf_email = secrets['EMAIL']
     hf_pass = secrets['PASS']
 
+    sys_prompt = ""
+    if sys_prompt_flag:
+        sys_prompt = input("Enter sys_prompt:\n")
+
     if dir_path:
         directory = os.fsencode(dir_path)
 
+        print('Started Dir Handling\n')
         for file in os.listdir(directory):
             filename = dir_path + ('' if dir_path.endswith('/') else '/') + os.fsdecode(file)
             with open(filename) as fd:
                 prompt = '\n'.join([line.rstrip() for line in fd])
-                generate_response(prompt, hf_email, hf_pass)
+                generate_response(prompt, hf_email, hf_pass, sys_prompt)
+        print('Stopped Dir Handling\n')
         return
 
     while True:
         prompt = input("Enter Prompt\n")
         if prompt == '--':
             return
-        generate_response(prompt, hf_email, hf_pass)
+        generate_response(prompt, hf_email, hf_pass, sys_prompt)
 
 
 if __name__ == '__main__':
     dir_path = None
+    sys_prompt = False
+
     if len(sys.argv) > 1:
-        if sys.argv[1] == 'h':
-            print('LLM: LLama-30b\n'
+        argv = sys.argv
+
+        if 'h' in argv:
+            print('LLM: LLama-70b. AVG Time: 30 s.\n'
+                  'Аргументы запуска:\n'
+                  'h - help\n'
+                  'pwd - Path to Prompts dir\n'
+                  's - Enable System Prompt\n\n'
                   'Положите в текущую папку файл hf.env, в котором '
                   'хранятся логин и пароль от HuggingFace в формате:\n'
                   'EMAIL=...\n'
@@ -58,6 +74,8 @@ if __name__ == '__main__':
                   '----------\n\n'
                   'Либо вводить Промпты напрямую, чтобы завершить напишите --')
             exit(0)
-        else:
-            dir_path = sys.argv[1]
-    run(dir_path)
+        if 's' in argv:
+            sys_prompt = True
+            argv.remove('s')
+        dir_path = sys.argv[1]
+    run(dir_path, sys_prompt)
